48f571e3372da11d1da2f7d10ee07db1a72eb9fa
Harry Scells <harryscells@gmail.com>
2022-05-04 16:24:48 +1000

    Add loading of CLEF TAR 2017 and 2018 collections.

--- src/hyperbool/experiments/collections.py
+++ src/hyperbool/experiments/collections.py
@@ -13,6 +13,8 @@ from ir_measures import Qrel
 
 from hyperbool import util
 
+__GITHASH_CLEFTAR = "8ce8a63bebb7d88f42dc1abad3e5744e315d07ae"
+
 
 @dataclass_json
 @dataclass
@@ -48,7 +50,7 @@ class Collection:
             qrels = list(ir_measures.read_trec_qrels(f))
         topics = list(Topic.from_file(topics_path))
 
-        return Collection(collection_path.name, topics, qrels)
+        return Collection(str(collection_path).replace(str(Path(appdirs.user_data_dir("hyperbool")) / "collections"), "")[1:], topics, qrels)
 
     def __hash__(self):
         return hash("".join([repr(topic) for topic in self.topics] +
@@ -57,6 +59,145 @@ class Collection:
 
 def load_collection(name: str) -> Collection:
     return __collection_load_methods[name](name)
+
+
+def parse_clef_tar_topic(topic_str: str, date_from: str = "1940", date_to: str = "2017", parse_query: bool = False) -> Topic:
+    topic_id = ""
+    topic_description = ""
+    topic_query = ""
+    parsing_query = False
+    for line in topic_str.splitlines():
+        if parse_query and parsing_query:
+            if len(line) == 0:
+                parsing_query = False
+                continue
+            topic_query += line
+        if line.startswith("Topic:"):
+            topic_id = line.replace("Topic: ", "")
+        if line.startswith("Title:"):
+            topic_description = line.replace("Title: ", "")
+        if parse_query and line.startswith("Query:"):
+            parsing_query = True
+
+    # Don't worry, the pubdates will be added back in later.
+    topic_query = topic_query.replace("Total references = 1551", "") \
+        .replace("“", '"') \
+        .replace("”", '"') \
+        .replace(" AND 1992/01/01:2015/11/30[crdt]", "") \
+        .replace(" AND 1940/01/01:2012/09/21[crdt]", "") \
+        .replace(" AND 1940/01/01:2015/02/28[crdt]", "") \
+        .replace(" AND 1966/01/01:2017/03/30[crdt]", "") \
+        .replace(" AND 1940/01/01:2016/01/19[crdt]", "")
+
+    return Topic(identifier=topic_id,
+                 description=topic_description,
+                 raw_query=topic_query,
+                 date_from=date_from,
+                 date_to=date_to)
+
+
+def __load_clef_tar_2017_training(name: str) -> Collection:
+    return __load_clef_tar(name=name,
+                           git_hash=__GITHASH_CLEFTAR,
+                           year=2017,
+                           subfolder="training",
+                           qrels_path="qrels/train.combined.qrels",
+                           topics_path="topics_train/",
+                           topic_ids=["1", "11", "14", "19", "23", "28", "33", "35", "37", "38",
+                                      "4", "43", "44", "45", "50", "53", "54", "55", "6", "9"],
+                           pubmed_topic_ids=["19", "35", "37", "38", "44"])
+
+
+def __load_clef_tar_2017_testing(name: str) -> Collection:
+    return __load_clef_tar(name=name,
+                           git_hash=__GITHASH_CLEFTAR,
+                           year=2017,
+                           subfolder="testing",
+                           qrels_path="qrels/test.combined.qrels",
+                           topics_path="topics/",
+                           topic_ids=["10", "12", "15", "16", "17", "18", "2", "21", "22", "25", "26",
+                                      "27", "29", "31", "32", "34", "36", "39", "40", "41", "42", "47",
+                                      "48", "49", "5", "51", "56", "57", "7", "8"],
+                           pubmed_topic_ids=["32"])
+
+
+def __load_clef_tar_2018_training(name: str) -> Collection:
+    return __load_clef_tar(name=name,
+                           git_hash=__GITHASH_CLEFTAR,
+                           year=2018,
+                           subfolder="Task2/Training/",
+                           qrels_path="qrels/full.train.abs.2018.qrels",
+                           topics_path="topics/",
+                           topic_ids=["CD007394", "CD007427", "CD008054", "CD008081", "CD008643", "CD008686",
+                                      "CD008691", "CD008760", "CD008782", "CD008803", "CD009020", "CD009135",
+                                      "CD009185", "CD009323", "CD009372", "CD009519", "CD009551", "CD009579",
+                                      "CD009591", "CD009593", "CD009647", "CD009786", "CD009925", "CD009944",
+                                      "CD010023", "CD010173", "CD010276", "CD010339", "CD010386", "CD010409",
+                                      "CD010438", "CD010542", "CD010632", "CD010633", "CD010653", "CD010705",
+                                      "CD011134", "CD011548", "CD011549", "CD011975", "CD011984", "CD012019"],
+                           pubmed_topic_ids=["CD009323", "CD010339", "CD011548", "CD011549", "CD008054", "CD009020"])
+
+
+def __load_clef_tar_2018_testing(name: str) -> Collection:
+    return __load_clef_tar(name=name,
+                           git_hash=__GITHASH_CLEFTAR,
+                           year=2018,
+                           subfolder="Task2/Testing/",
+                           qrels_path="qrels/full.test.abs.2018.qrels",
+                           topics_path="topics/",
+                           topic_ids=["CD008122", "CD008587", "CD008759", "CD008892", "CD009175", "CD009263",
+                                      "CD009694", "CD010213", "CD010296", "CD010502", "CD010657", "CD010680",
+                                      "CD010864", "CD011053", "CD011126", "CD011420", "CD011431", "CD011515",
+                                      "CD011602", "CD011686", "CD011912", "CD011926", "CD012009", "CD012010",
+                                      "CD012083", "CD012165", "CD012179", "CD012216", "CD012281", "CD012599"],
+                           pubmed_topic_ids=["CD008587", "CD009263", "CD011420", "CD011912", "CD011926"],
+                           pubdates_path="2018-TAR/Task1/Testing/pubdates.txt")
+
+
+def __load_clef_tar(name: str, git_hash: str, year: int, subfolder: str,
+                    qrels_path: str, topics_path: str, topic_ids: List[str],
+                    pubmed_topic_ids: List[str] = None, pubdates_path: str = None) -> Collection:
+    collection_base_url = f"https://raw.githubusercontent.com/CLEF-TAR/tar/{git_hash}/"
+    collection_url = f"https://raw.githubusercontent.com/CLEF-TAR/tar/{git_hash}/{year}-TAR/{subfolder}/"
+    qrels_url = collection_url + qrels_path
+
+    download_dir = Path(appdirs.user_data_dir("hyperbool")) / "collections" / name
+    raw_collection = download_dir / "raw"
+    topic_file = download_dir / "topics.jsonl"
+    qrels_file = download_dir / "qrels"
+
+    pubdates = {}
+
+    if not download_dir.exists():
+        os.makedirs(download_dir, exist_ok=True)
+        os.makedirs(raw_collection, exist_ok=True)
+        util.download_file(qrels_url, qrels_file)
+
+        if pubdates_path is not None:
+            util.download_file(collection_base_url + pubdates_path, download_dir / "pubdates.txt")
+            with open(download_dir / "pubdates.txt", "r") as f:
+                for line in f:
+                    topic, date_from, date_to = line.split()
+                    pubdates[topic] = (f"{date_from[:4]}/{date_from[4:6]}/{date_from[6:]}",
+                                       f"{date_to[:4]}/{date_to[4:6]}/{date_to[6:]}")
+
+        with open(topic_file, "w") as f:
+            for topic in topic_ids:
+                date_from = "1940/01/01"
+                date_to = f"{year}/01/01"
+
+                if topic in pubdates:
+                    date_from = pubdates[topic][0]
+                    date_to = pubdates[topic][1]
+
+                util.download_file(f"{collection_url}{topics_path}{topic}", raw_collection / topic)
+                with open(raw_collection / topic, "r") as g:
+                    f.write(parse_clef_tar_topic(topic_str=g.read(),
+                                                 date_from=date_from,
+                                                 date_to=date_to,
+                                                 parse_query=topic in pubmed_topic_ids).to_json() + "\n")
+
+    return Collection.from_dir(download_dir)
 
 
 def __load_sysrev_seed(name: str) -> Collection:
@@ -109,5 +250,21 @@ def __load_sysrev_seed(name: str) -> Collection:
 
 
 __collection_load_methods = {
-    "ielab/sysrev-seed-collection": __load_sysrev_seed
+    # -------------------------------------------------------------------------------------------
+    # For the sysrev-seed collection, we can use all the queries.
+    "ielab/sysrev-seed-collection": __load_sysrev_seed,
+    # -------------------------------------------------------------------------------------------
+    # For CLEF TAR 2017 and 2018, the non-Pubmed queries are filtered out.
+    "clef-tar/2017/training": __load_clef_tar_2017_training,
+    "clef-tar/2017/testing": __load_clef_tar_2017_testing,
+    "clef-tar/2018/training": __load_clef_tar_2018_training,
+    "clef-tar/2018/testing": __load_clef_tar_2018_testing
+    # -------------------------------------------------------------------------------------------
+    # For CLEF TAR 2019, there are no additional topics that contain new Pubmed queries.
+    # -------------------------------------------------------------------------------------------
+    # Other possible datasets include:
+    # - https://github.com/Amal-Alharbi/Systematic_Reviews_Update (but no Pubmed queries)
+    # - https://github.com/ielab/SIGIR2017-SysRev-Collection (only about a dozen Pubmed queries from 125 in total)
+    # -------------------------------------------------------------------------------------------
+
 }
