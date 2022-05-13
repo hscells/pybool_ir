4f933c7b0fdbd532e557b20f3d7d27213845aba7
Harry Scells <harryscells@gmail.com>
2022-05-13 13:36:27 +1000

    Update installation scripts.

--- install_pylucene.sh
+++ install_pylucene.sh
@@ -1,5 +1,14 @@
+# --------------------------------------------
 # Because pylucene is a pain to install,
 # this script will try to install it for you.
+# --------------------------------------------
+
+# https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Change these variables as needed.
 mirror=downloads
@@ -17,18 +26,22 @@ wget https://${mirror}.apache.org/ant/binaries/apache-ant-${ant_version}-bin.zip
 unzip apache-ant-${ant_version}-bin.zip
 rm apache-ant-${ant_version}-bin.zip
 mv apache-ant-${ant_version} ant
+export PATH="$PATH:$(pwd)/ant/bin"
 
 # Install jcc.
+# https://lucene.apache.org/pylucene/jcc/install.html
 pipenv shell
 cd pylucene
 pushd jcc
 python setup.py build
+python setup.py install
 popd
 
 # Install pylucene.
-export PATH="$PATH:$(pwd)/ant/bin"
+# https://lucene.apache.org/pylucene/install.html
 ANT=$(which ant) PYTHON=$(which python) JCC="python -m jcc --shared --arch x86_64 --wheel" NUM_FILES=10 make
 
 # Clean up the files.
+mv dist/lucene*.whl dist/lucene.whl
 cd ../
-rm -rf ant
+pipenv update # Not required if using something other than pipenv.
