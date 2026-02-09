70e0397cb414dd9a27c24d02ec82f1f385f6a1c1
Harry Scells <harry@scells.me>
2026-02-09 23:01:17 +0100

    Sync first.

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,4 +1,5 @@
 pip install uv
+uv sync
 uv add /pybool_ir/pylucene/dist/*.whl
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export JCC_JDK=${JAVA_HOME}
@@ -7,4 +8,3 @@ export JCC_LFLAGS="-L$JAVA_HOME/lib;-ljava;-L$JAVA_HOME/lib/server;-ljvm;-Wl,-rp
 export JCC="python -m jcc --wheel"
 uv add /pybool_ir/pylucene/jcc
 uv pip install -e .
-uv sync
